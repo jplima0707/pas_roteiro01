a476f3de31838d07be6fbcc816a96ea79fb7179d
João Pedro Benevides Lima <127553965+jplima0707@users.noreply.github.com>
2025-03-06 23:23:08 +0000

    init

---
+++ projeto1a/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main (String [] args) throws Exception {
+        System.out.println("\n Hello!");
+        String version = System.getProperty("java.version");
+        System.out.println("Version: " + version);
+    }
+}
